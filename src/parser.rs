a063c4feea9d69e7687f08abdcacd6a760edd850
Emily Crandall Fleischman <emilycf@mit.edu>
2018-12-21 09:53:09 -0800

    Streamline unary operator and variable parsing

--- src/parser.rs
+++ src/parser.rs
@@ -124,9 +124,14 @@ where
             None => Err(ParserError::EndOfInput(self.line()))?,
             Some(Token::LeftParen) => self.parse_parentheses(),
             Some(Token::Number(num)) => Ok(Expression::Value(Value::Number(num))),
-            Some(Token::Tag(tag)) => match self.variables.get(&tag) {
-                Some(&exp) => Ok(Expression::Value(exp)),
-                None => self.parse_unary(tag),
+            Some(Token::Tag(tag)) => match UnaryBuiltins.get(&tag) {
+                Some(op) => op
+                    .expression(self.parse_expression_1(op.precedence)?)
+                    .map_err(|err| ParserError::Math(err, self.line()).into()),
+                None => match self.variables.get(&tag) {
+                    Some(&value) => Ok(Expression::Value(value)),
+                    None => Ok(Expression::Variable(tag)),
+                },
             },
             Some(tok) => Err(ParserError::Token(tok, self.line()))?,
         }
@@ -148,16 +153,6 @@ where
             }
             Some(tok) => Err(ParserError::Token(tok, self.line()))?,
             None => Err(ParserError::Parentheses(start_line))?,
-        }
-    }
-
-    fn parse_unary(&mut self, tag: String) -> EResult<Expression> {
-        if let Some(op) = UnaryBuiltins.get(&tag) {
-            let arg = self.parse_expression_1(op.precedence)?;
-            op.expression(arg)
-                .map_err(|err| ParserError::Math(err, self.line()).into())
-        } else {
-            Err(ParserError::Token(Token::Tag(tag), self.line()))?
         }
     }
 }
